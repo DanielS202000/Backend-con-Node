7eed6aac741723c47a33e6d66552aac7f336c649
DanielS202000 <danielsamm62@gmail.com>
2023-06-05 02:38:14 -0600

    Codigos de estado o HTTP response status codes

--- routes/products.router.js
+++ routes/products.router.js
@@ -21,18 +21,24 @@ router.get('/filter', (req, res )=>{
   res.send("Yo soy un filtro");
 });
 
-router.get('/:id', (req, res)=>{
-  const { id }= req.params;
-  res.json ({
-    id,
-    name: 'Producto 2',
-    price: 2000
-  });
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+  if (id === '999') {
+    res.status(404).json({
+      message: 'not fould'
+    });
+  } else {
+    res.status(200).json({
+      id,
+      name: 'Producto 2',
+      price: 2000
+    });
+  }
 });
 
 router.post('/', (req, res ) => {
   const body = req.body;
-  res.json({
+  res.status(201).json({
     message: 'created',
     data: body
   });
