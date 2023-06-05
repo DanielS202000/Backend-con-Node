1f657a9beb105f5e4294d6f7eac6f3b328b87dea
DanielS202000 <danielsamm62@gmail.com>
2023-06-05 00:28:26 -0600

    GET_recibir parametros

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const {faker } = require ('@faker-js/faker');
+
 const app = express();
 const port = 3000;
 
@@ -11,11 +13,48 @@ app.get('/nueva-ruta', (req, res) => {
 });
 
 app.get('/products', (req, res) => {
-  res.json({
-    name: 'Producto 1',
-    price: 1000
+  const products= [];
+  const { size } = req.query;
+  const limit = size || 10;
+  for (let index = 0; index < limit; index++){
+    products.push({
+      name: faker.commerce.productName(),
+      price: parseInt(faker.commerce.price(), 10),
+      image: faker.image.imageUrl(),
+
+    });
+  }
+  res.json(products);
+});
+
+app.get('/products/:id', (req, res)=>{
+  const { id }= req.params;
+  res.json ({
+    id,
+    name: 'Producto 2',
+    price: 2000
   });
 });
+
+app.get('/users', (req, res) =>{
+  const {limit, offset} = req.query;
+  if (limit && offset){
+    res.json({
+      limit,
+      offset
+    });
+  }else{
+    res.send('No hay parametros')
+  }
+});
+
+app.get('/categories/:categoryId/products/:productId', (req, res ) =>{
+  const { categoryId, productId }= req.params;
+  res.json ({
+    categoryId,
+    productId
+  });
+})
 
 app.listen(port, () =>{
   console.log('Mi port ' + port);
